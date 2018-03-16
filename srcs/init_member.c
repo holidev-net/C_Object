890c9fb69088a4f30c90fee630a5df7c96989367
Alexandre Chamard-bois <alexandre.chamard-bois@epitech.eu>
2018-03-16 17:58:13 +0100

    space->tab

--- srcs/init_member.c
+++ srcs/init_member.c
@@ -26,39 +26,40 @@ static const short asmmov_g[] = {
 	0xbb48, // rbx
 };
 
+static const unsigned char  buf[] = {
+	0x48, 0xb8, // mov rax
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // addr of func
+	0x00, 0x00, // mov last arg
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // addr obj
+	0xff, 0xe0, // jmp rax
+};
+
 int init_members(void *obj, int nb_functs, ...)
 {
-        init_wrap_t     cur_wrapper;
-        va_list         vl;
-	static const unsigned char  buf[] = {
-		0x48, 0xb8, // mov rax
-		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // addr of func
-		0x00, 0x00, // mov last arg
-		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // addr obj
-		0xff, 0xe0, // jmp rax
-	};
-        const size_t  alignedSize = (sizeof(buf) % 16 == 0 
-                        ? sizeof(buf) 
-                        : sizeof(buf) + (sizeof(buf) - (sizeof(buf) % ALIGN)));
+	init_wrap_t     cur_wrapper;
+	va_list         vl;
+	const size_t  alignedSize = (sizeof(buf) % 16 == 0
+			? sizeof(buf)
+			: sizeof(buf) + (sizeof(buf) - (sizeof(buf) % ALIGN)));
 	void    *virtualCodeAddress = mmap(NULL, alignedSize + ALIGN,
-		        PROT_READ | PROT_WRITE | PROT_EXEC,
-		        MAP_ANONYMOUS | MAP_PRIVATE, 0, 0);
-        void    *cur = virtualCodeAddress + ALIGN;
+			PROT_READ | PROT_WRITE | PROT_EXEC,
+			MAP_ANONYMOUS | MAP_PRIVATE, 0, 0);
+	void    *cur = virtualCodeAddress + ALIGN;
 
-        if (virtualCodeAddress == (void*) -1)
-                return (-1);
-        va_start(vl, nb_functs);
+	if (virtualCodeAddress == (void*) -1)
+		return (-1);
+	va_start(vl, nb_functs);
 	*((size_t*) virtualCodeAddress) = alignedSize + ALIGN;
-        virtualCodeAddress = cur;
-        while (nb_functs--) {
-                cur_wrapper = va_arg(vl, init_wrap_t);
-                memcpy(cur, buf, sizeof(buf));
-                *((void **) (cur + 2)) = cur_wrapper.target;
-                *((short *) (cur + 10)) = asmmov_g[cur_wrapper.nb_args];
-                *((void **) (cur + 12)) = obj;
-                *(cur_wrapper.funct) = cur;
-                cur += alignedSize;
-        }
+	virtualCodeAddress = cur;
+	while (nb_functs--) {
+		cur_wrapper = va_arg(vl, init_wrap_t);
+		memcpy(cur, buf, sizeof(buf));
+		*((void **) (cur + 2)) = cur_wrapper.target;
+		*((short *) (cur + 10)) = asmmov_g[cur_wrapper.nb_args];
+		*((void **) (cur + 12)) = obj;
+		*(cur_wrapper.funct) = cur;
+		cur += alignedSize;
+	}
 	return (0);
 }
 
