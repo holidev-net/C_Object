f9f881f1cc6c65c0bf14007a2594b9c0f7c8e89c
Viguier Benjamin <benjamin.viguier@epitech.eu>
2018-03-16 18:31:06 +0100

    add protect to generated executable memory

--- srcs/init_member.c
+++ srcs/init_member.c
@@ -13,7 +13,7 @@
 
 #define ALIGN   16
 
-static const short asmmov_g[] = {
+static const short _asmmov_g[] = {
 	0xbf48, // rdi
 	0xbe48, // rsi
 	0xba48, // rdx
@@ -26,40 +26,50 @@ static const short asmmov_g[] = {
 	0xbb48, // rbx
 };
 
-int init_members(void *obj, int nb_functs, ...)
-{
-        init_wrap_t     cur_wrapper;
-        va_list         vl;
-	static const unsigned char  buf[] = {
-		0x48, 0xb8, // mov rax
-		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // addr of func
-		0x00, 0x00, // mov last arg
-		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // addr obj
-		0xff, 0xe0, // jmp rax
-	};
-        const size_t  alignedSize = (sizeof(buf) % 16 == 0 
-                        ? sizeof(buf) 
-                        : sizeof(buf) + (sizeof(buf) - (sizeof(buf) % ALIGN)));
-	void    *virtualCodeAddress = mmap(NULL, alignedSize + ALIGN,
-		        PROT_READ | PROT_WRITE | PROT_EXEC,
-		        MAP_ANONYMOUS | MAP_PRIVATE, 0, 0);
-        void    *cur = virtualCodeAddress + ALIGN;
+static const unsigned char  _caller_template_g[] = {
+	0x48, 0xb8, // mov rax
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // addr of func
+	0x00, 0x00, // mov last arg
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // addr obj
+	0xff, 0xe0, // jmp rax
+};
 
-        if (virtualCodeAddress == (void*) -1)
-                return (-1);
-        va_start(vl, nb_functs);
-	*((size_t*) virtualCodeAddress) = alignedSize + ALIGN;
-        virtualCodeAddress = cur;
+static const size_t  _alignedSize_g = (sizeof(_caller_template_g) % 16 == 0 
+                        ? sizeof(_caller_template_g) 
+                        : sizeof(_caller_template_g) + (sizeof(_caller_template_g) - (sizeof(_caller_template_g) % ALIGN)));
+
+static inline int init_member_write_member_callers(
+	void *obj, va_list vl, void *caller_address, int nb_functs)
+{
+	init_wrap_t     cur_wrapper;
+	void		*cur = caller_address + ALIGN;
+
+	*((size_t*) caller_address) = _alignedSize_g + ALIGN;
         while (nb_functs--) {
                 cur_wrapper = va_arg(vl, init_wrap_t);
-                memcpy(cur, buf, sizeof(buf));
+                memcpy(cur, _caller_template_g, sizeof(_caller_template_g));
                 *((void **) (cur + 2)) = cur_wrapper.target;
-                *((short *) (cur + 10)) = asmmov_g[cur_wrapper.nb_args];
+                *((short *) (cur + 10)) = _asmmov_g[cur_wrapper.nb_args];
                 *((void **) (cur + 12)) = obj;
                 *(cur_wrapper.funct) = cur;
-                cur += alignedSize;
+                cur += _alignedSize_g;
         }
-	return (0);
+	va_end(vl);
+	return (mprotect(caller_address, ALIGN + _alignedSize_g, PROT_EXEC));
+}
+
+int init_members(void *obj, int nb_functs, ...)
+{
+        va_list         vl;
+	void    *caller_address = mmap(NULL, _alignedSize_g + ALIGN,
+		        PROT_READ | PROT_WRITE | PROT_EXEC,
+		        MAP_ANONYMOUS | MAP_PRIVATE, 0, 0);
+
+        if (caller_address == (void*) -1)
+                return (-1);
+        va_start(vl, nb_functs);
+	return (init_member_write_member_callers(
+		obj, vl, caller_address, nb_functs));
 }
 
 void delete_members(void *first_member)
